5e2c2790aa7e6d94350835500a97f5f2bd60782a
SoftyNg <thelaw111@gmail.com>
2023-11-12 18:54:29 +0100

    Setup Dependences
    Generated dependency files using spring initializer, so that we can use RESTful webservices plus H2 Database and JPA

---
+++ geobyteback/src/main/java/com/app/geobyte/GeobyteApplication.java
@@ -0,0 +1,13 @@
+package com.app.geobyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeobyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeobyteApplication.class, args);
+	}
+
+}
---
+++ geobyteback/src/test/java/com/app/geobyte/GeobyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.geobyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeobyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
